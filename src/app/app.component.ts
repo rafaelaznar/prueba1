42a5f63eab893a5b8a87c270bc583c3c5c258edc
Rafael Aznar <rafaaznar@gmail.com>
2024-09-24 21:19:21 +0200

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'prueba1';
+  title: string = 'prueba1';
+  numero_de_repeticiones: number = 0;
+  repetir: boolean = false;
+  nombre: string = 'rafa';
+  personas:any = [];
+
+  constructor(private oHttp: HttpClient) {
+    this.repetir = true;
+    this.numero_de_repeticiones = 10;
+    this.title = 'prueba de DAW';
+    this.getNombres();
+  }
+
+  getNombres(): void {
+    this.oHttp.get('http://localhost:8085/persona/genera/10').subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.personas = data;
+      },
+      error: (error: any) => {
+        console.log(error);
+      }
+    })
+  }
+
+
 }
